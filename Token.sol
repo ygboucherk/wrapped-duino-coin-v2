af9771da90ee555049b2705248dde936c94f391b
Yanis Boucherk <ygboucherk@gmail.com>
2021-01-09 17:07:29 +0100

    Add files via upload

---
+++ Token.sol
@@ -0,0 +1,21 @@
+// 0.5.1-c8a2
+// Enable optimization
+pragma solidity ^0.5.0;
+
+import "./ERC20.sol";
+import "./ERC20Detailed.sol";
+
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+contract Token is ERC20, ERC20Detailed {
+
+    /**
+     * @dev Constructor that only says hello xD
+     */
+    constructor () public ERC20Detailed("Wrapped Duino-Coin v2", "wDUCO", 6) {
+    }
+}
